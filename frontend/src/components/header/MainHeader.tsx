a36ffb96354cb756f803b10b72fc068bc4cc3cc2
marcin <persky03@gmail.com>
2023-02-09 01:59:17 +0100

    feat: header (nav) without rwd and static

--- frontend/src/components/header/MainHeader.tsx
+++ frontend/src/components/header/MainHeader.tsx
@@ -4,7 +4,22 @@ export interface IMainHeader {
 }
 
 const MainHeader: React.FC<IMainHeader> = ({ sampleTextProp }) => {
-  return <div className="bg-red-600">{sampleTextProp}</div>;
+  console.log(sampleTextProp);//Temporary logging
+  return <div className="flex justify-between gap-2 mx-4 mt-4 items-center">
+    <div>
+    <img src="/img/seoBarista_black.svg" alt="SEOBarista Logo"/>
+      </div>
+    <div className="navigation flex gap-2 items-center">
+        <ul className="flex gap-8 text-xs items-center">
+        <li>Blog</li>
+        <li>Pricing</li>
+        <li>Login</li>
+        <li className="bg-yellow-400 p-2">Try for free</li>
+        </ul>
+    </div>
+   
+    
+    </div>;
 };
 
 export default MainHeader;
